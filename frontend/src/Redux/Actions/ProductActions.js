1e68512a05c56c6e24d1c09ce364d9f8aac5b14f
unknown <henokmekuanint79@gmail.com>
2022-08-07 17:08:18 +0300

    loading_Scene_added

--- frontend/src/Redux/Actions/ProductActions.js
+++ frontend/src/Redux/Actions/ProductActions.js
@@ -1,5 +1,18 @@
-
+import axios from "axios"
+import {PRODUCTION_LIST_REQUEST, 
+    PRODUCTION_LIST_SUCCESS, 
+    PRODUCTION_LIST_FAIL} from "../Constants/ProductConstants"
 export const listProduct = ()=> async (dispatch)=>{
-
-
+    try{
+        dispatch({type:PRODUCTION_LIST_REQUEST})
+        const {data}=await axios.get("api/products");
+        dispatch({type:PRODUCTION_LIST_SUCCESS,payload:data})
+    }catch(error){
+        dispatch({
+            type:PRODUCTION_LIST_FAIL,
+            payload:error.response && error.response.data.message 
+            ? error.response.data.message
+            :error.message,
+        })
+    }
 }
--- frontend/src/components/homeComponents/ShopSection.js
+++ frontend/src/components/homeComponents/ShopSection.js
@@ -2,20 +2,19 @@ import React, { useEffect,useState } from "react";
 import { Link } from "react-router-dom";
 import Rating from "./Rating";
 import Pagination from "./pagination";
-import axios from "axios";
+import {useDispatch,useSelector} from "react-redux";
+import { listProduct } from "../../Redux/Actions/ProductActions";
+
 
 const ShopSection = () => {
-  const [products,setProducts]=useState([]);
+  const dispatch=useDispatch();
+
+  const productList=useSelector((state)=>state.productList);
+  const {loading,error,products}=productList;
 
   useEffect(()=>{
-    const fetchproducts=async()=>{
-      const {data}= await axios.get("/api/products")
-      setProducts(data);
-    };
-    fetchproducts();
-  }
-
- ,[] );
+    dispatch(listProduct())
+  },[dispatch] );
   return (
     <>
       <div className="container">
@@ -23,7 +22,12 @@ const ShopSection = () => {
           <div className="row">
             <div className="col-lg-12 col-md-12 article">
               <div className="shopcontainer row">
-                {products.map((product) => (
+                {
+                  loading ? (<p>Loading...</p>) : error?(<p>Error :{error}</p>)
+                  :
+                  (
+                    <>
+{products.map((product) => (
                   <div
                     className="shop col-lg-4 col-md-6 col-sm-6"
                     key={product._id}
@@ -51,6 +55,11 @@ const ShopSection = () => {
                     </div>
                   </div>
                 ))}
+                    </>
+                  )
+
+                }
+                
                 {/* Pagination */}
                 <Pagination />
               </div>
