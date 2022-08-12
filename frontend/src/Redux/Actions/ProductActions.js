d3dc05008fb7138901fad3fd661c7b6a1351af23
unknown <henokmekuanint79@gmail.com>
2022-08-12 19:12:41 +0300

    WORKING

--- frontend/src/Redux/Actions/ProductActions.js
+++ frontend/src/Redux/Actions/ProductActions.js
@@ -16,3 +16,18 @@ export const listProduct = ()=> async (dispatch)=>{
         })
     }
 }
+
+export const listProductDetail = (id)=> async (dispatch)=>{
+    try{
+        dispatch({type:PRODUCTION_LIST_REQUEST})
+        const {data}=await axios.get("api/products");
+        dispatch({type:PRODUCTION_LIST_SUCCESS,payload:data})
+    }catch(error){
+        dispatch({
+            type:PRODUCTION_LIST_FAIL,
+            payload:error.response && error.response.data.message 
+            ? error.response.data.message
+            :error.message,
+        })
+    }
+}
--- frontend/src/Redux/Reducers/ProductReducers.js
+++ frontend/src/Redux/Reducers/ProductReducers.js
@@ -1,4 +1,5 @@
 import { PRODUCTION_LIST_REQUEST ,PRODUCTION_LIST_SUCCESS,PRODUCTION_LIST_FAIL} from "../Constants/ProductConstants";
+import { PRODUCTION_DETAIL_REQUEST ,PRODUCTION_DETAIL_SUCCESS,PRODUCTION_DETAIL_FAIL} from "../Constants/ProductConstants";
 
 export const productListReducer=(state={products:[]},action)=>{
     switch(action.type){
@@ -7,6 +8,19 @@ export const productListReducer=(state={products:[]},action)=>{
         case PRODUCTION_LIST_SUCCESS:
             return { loading:false, products:action.payload};
         case PRODUCTION_LIST_FAIL:
+            return { loading:false,error:action.payload};
+        default:
+            return state;
+    }
+}
+
+export const productDetailReducer=(state={products:{reviews:[]}},action)=>{
+    switch(action.type){
+        case PRODUCTION_DETAIL_REQUEST:
+            return { ...state,loading:true };
+        case PRODUCTION_DETAIL_SUCCESS:
+            return { loading:false, product:action.payload};
+        case PRODUCTION_DETAIL_FAIL:
             return { loading:false,error:action.payload};
         default:
             return state;
