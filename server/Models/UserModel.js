b67a820da71754cc696cc5fd299abb6e43caca5c
unknown <henokmekuanint79@gmail.com>
2022-08-19 13:41:45 +0300

    register_api_Added

--- server/Models/UserModel.js
+++ server/Models/UserModel.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs"
-const userSchema=mongoose.Schema({
+const userSchema=mongoose.Schema(
+    {
 
     name:{
         type:String,
@@ -28,6 +29,14 @@ const userSchema=mongoose.Schema({
 
 userSchema.methods.matchPasswords = async function (enterPassword){
      return await bcrypt.compare(enterPassword,this.password);
-}
+};
+
+userSchema.pre("save",async function(next){
+    if (!this.isModified("password")){
+        next();
+    }
+    const salt =await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt)
+})
 const User= mongoose.model("User",userSchema)
 export default User
--- server/Routes/UserRoutes.js
+++ server/Routes/UserRoutes.js
@@ -30,11 +30,40 @@ userRoute.post(
     })
 );
 
+userRoute.post(
+    "/",
+    asyncHandler(async (req,res) => {
+        const { name,email,password} =req.body;
+        const userExists = await User.findOne({ email })
+        if ( userExists ) {
+            res.status(400)
+            throw new Error("User already exists")
+        }
+const user =await User.create({
+            name,
+            email,
+            password
+            });
+if (user) {
+    res.status(201).json({
+        _id:user._id,
+        name:user.name,
+        email:user.email,
+        isAdmin:user.isAdmin,
+        token:generateToken(user._id),
+    })
+}
+else{
+    res.status(400);
+    throw new Error ("Invalid User Data")
+}
+    })
+);
 
 userRoute.get(
     "/profile",
     protect,
-    asyncHandler(async (req,res)=>{
+    asyncHandler(async (req,res) => {
         const user = await User.findById(req.user._id);
         if (user) {
             res.json({
@@ -52,18 +81,6 @@ userRoute.get(
 );
 
 
-userRoute.post(
-    "/",
-    protect,
-    asyncHandler(async (req,res)=>{
-        const {name,email,password} =req.body;
-        const userExits = await User.findOne({ email })
-        const user =await User.findOne({ email });
-        if (condition) {
-            
-        }
-    })
-);
 
 
 
