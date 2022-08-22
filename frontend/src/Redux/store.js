ee2c026088b5c1eaa1715aff03b61f53851b257e
unknown <henokmekuanint79@gmail.com>
2022-08-22 11:52:54 +0300

    user_action_added

--- frontend/src/Redux/store.js
+++ frontend/src/Redux/store.js
@@ -3,20 +3,28 @@ import thunk from "redux-thunk";
 import { composeWithDevTools} from "redux-devtools-extension";
 import { productListReducer,productDetailsReducer } from "./Reducers/ProductReducers";
 import { cartReducer } from "./Reducers/CartReducer";
+import { userLoginReducer } from "./Reducers/userReducer";
 const reducer = combineReducers({
     productList:productListReducer,
     productDetails:productDetailsReducer,
     cart:cartReducer,
+    userLogin:userLoginReducer
 
 })
 
 const cartItemsFromLocalStorage = localStorage.getItem("cartItems")
 ? JSON.parse(localStorage.getItem("cartItems"))
 : []
+
+const userInfoFromLocalStorage = localStorage.getItem("userInfo")
+? JSON.parse(localStorage.getItem("userInfo"))
+: null
+
 const initialState={
     cart:{
         cartItems:cartItemsFromLocalStorage
-    }
+    },
+    userLogin:{userInfo:userInfoFromLocalStorage }
 }
 const middleware=[thunk]  
 const store=createStore(
