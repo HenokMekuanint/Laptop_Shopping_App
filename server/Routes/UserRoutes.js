4415f463f292ad4ae243b7f8eeb6d1b5fba437e0
unknown <henokmekuanint79@gmail.com>
2022-08-23 13:34:50 +0300

    user_profile_Detail

--- server/Routes/UserRoutes.js
+++ server/Routes/UserRoutes.js
@@ -71,7 +71,8 @@ userRoute.get(
                 name:user.name,
                 email:user.email,
                 isAdmin:user.isAdmin,
-                createdAt:user.createdAt,               
+                createdAt:user.createdAt, 
+                              
             })
         } else {
             res.status(401);
@@ -80,7 +81,36 @@ userRoute.get(
     })
 );
 
+//UPDATEPROFILE
+userRoute.get(
+    "/profile",
+    protect,
+    asyncHandler(async (req,res) => {
+        const user = await User.findById(req.user._id);
+        if (user) {
+            user.name=req.body.name || user.name
+            user.email=req.body.email || user.email
+            if (req.body.password) {
+                user.password=req.body.password
+            }
+        const updateUser = await user.save();
+        res.json({
+            _id:updateUser._id,
+            name:updateUser.name,
+            email:updateUser.email,
+            isAdmin:updateUser.isAdmin,
+            createdAt:updateUser.createdAt,
+            token:generateToken(updateUser._id),
+  
 
+        })
+
+        } else {
+            res.status(401);
+            throw new Error("User  not found");
+        }
+    })
+);
 
 
 
